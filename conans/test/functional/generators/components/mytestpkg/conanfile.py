c2e5a9cb806e1d1331e2895e455bde7b4c2093d8
Daniel Petry <daniel.petry@hella.com>
2021-03-29 14:48:26 +0200

    Create test package to recreate error

--- conans/test/functional/generators/components/mytestpkg/conanfile.py
+++ conans/test/functional/generators/components/mytestpkg/conanfile.py
@@ -5,39 +5,16 @@ class MytestpkgConan(ConanFile):
     name = "mytestpkg"
     version = "0.0.1"
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
-    generators = "cmake"
-
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
-#     def source(self):
-#         self.run("git clone https://github.com/conan-io/hello.git")
-#         # This small hack might be useful to guarantee proper /MT /MD linkage
-#         # in MSVC if the packaged project doesn't have variables to set it
-#         # properly
-#         tools.replace_in_file("hello/CMakeLists.txt", "PROJECT(HelloWorld)",
-#                               '''PROJECT(HelloWorld)
-# include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-# conan_basic_setup()''')
-
-#    def build(self):
-#        cmake = CMake(self)
-#        cmake.configure(source_folder="hello")
-#        cmake.build()
-
-        # Explicit way:
-        # self.run('cmake %s/hello %s'
-        #          % (self.source_folder, cmake.command_line))
-        # self.run("cmake --build . %s" % cmake.build_config)
+    generators = "cmake_find_package"
+    exports = "required.lib", "not_required.lib"
 
     def package(self):
         self.copy("*.lib", dst="lib", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.components["required_component"].names["cmake_find_package"] = "required_component"
-        self.cpp_info.components["not_required_component"].names["cmake_find_package"] = "not_required_component"
+        self.cpp_info.components["required"].libs = ["required"]                             
+        self.cpp_info.components["required"].names["cmake_find_package"] = "required"        
+        self.cpp_info.components["not_required"].libs = ["not_required"]                     
+        self.cpp_info.components["not_required"].names["cmake_find_package"] = "not_required"
 
 
